b2378b17d3be82eca4561a419ca9cf59999628ab
njgibbon <njgibbon@outlook.com>
2021-09-16 23:32:48 +0100

    basic test framework added

---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+import sys
+import unittest
+
+import file_dir_set_ops.main
+
+
+class TestMain(unittest.TestCase):
+    def test_print_items_with_newline(self):
+        items = ['a', 'b']
+        file_dir_set_ops.main.print_items_with_newline(items)
